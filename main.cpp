9919c905ebc406a7903646f026a9fa8fe6dbf4ea
kek228 <xyets777@gmail.com>
2019-11-24 13:59:19 +0300

    Все ноды на расстоянии к от заданной

--- main.cpp
+++ main.cpp
@@ -13,13 +13,78 @@
 #include <fstream>
 
 using namespace std;
-
 struct Node {
     int data;
     Node *l;
     Node *r;
 };
 
+void findDown(Node *node, int k, vector<int> &res) {
+    if (!node)
+        return;
+    if (k == 0) {
+        res.push_back(node->data);
+        return;
+    }
+    findDown(node->l, k - 1, res);
+    findDown(node->r, k - 1, res);
+}
+
+bool kdst(Node *node, int val, int &k, vector<int> &res) {
+    if (!node)
+        return false;
+    if (node->data == val) {
+        findDown(node->l, k - 1, res);
+        findDown(node->r, k - 1, res);
+        return true;
+    }
+    auto fl = kdst(node->l, val, k, res);
+    auto fr = kdst(node->r, val, k, res);
+    if (fl) {
+        --k;
+        if (k == 0)
+            res.push_back(node->data);
+        else
+            findDown(node->r, k - 1, res);
+    }
+    if (fr) {
+        --k;
+        if (k == 0)
+            res.push_back(node->data);
+        else
+            findDown(node->l, k - 1, res);
+    }
+    return fl || fr;
+}
+
+vector<int> distanceK(Node *root, Node *target, int K) {
+    vector<int> res;
+    if (K == 0) {
+        res.push_back(target->data);
+        return res;
+    }
+
+    kdst(root, target->data, K, res);
+    return res;
+}
+
 int main() {
+    auto *root = new Node{1, nullptr, nullptr};
+    root->l = new Node{3, nullptr, nullptr};
+    root->r = new Node{2, nullptr, nullptr};
+    //
+    root->l->l = new Node{9, nullptr, nullptr};
+    root->l->l->l = new Node{12, nullptr, nullptr};
+    root->l->r = new Node{7, nullptr, nullptr};
+    root->l->r->l = new Node{8, nullptr, nullptr};
+    root->l->r->l->r = new Node{6, nullptr, nullptr};
+    root->l->r->l->r->r = new Node{11, nullptr, nullptr};
+    //
+    root->r->r = new Node{10, nullptr, nullptr};
+    root->r->l = new Node{4, nullptr, nullptr};
+    root->r->l->r = new Node{5, nullptr, nullptr};
+    auto res = distanceK(root, root->l->r, 3);
+    for (auto n: res)
+        cout << n << ' ';
     return 0;
 }
--- trees/trees.h
+++ trees/trees.h
@@ -6,6 +6,7 @@ struct Node {
     Node *r;
 };
 
+/* ДИАМЕТР ДЕРЕВА */
 int treeDiam(Node *node, int &res) {
     if (!node)
         return 0;
@@ -15,9 +16,8 @@ int treeDiam(Node *node, int &res) {
     return max(ldst, rdst) + 1;
 }
 
-// kй сверху наследник
+/* kй сверху наследник */
 Node *res;
-
 void findKAns(Node *node, int val, int &k, bool &found) {
     if (!node)
         return;
@@ -51,3 +51,71 @@ void findKAns(Node *node, int val, int &k, bool &found) {
 //root->l->r->l->r = new Node{9, nullptr, nullptr};
 //root->l->r->l->r->r = new Node{10, nullptr, nullptr};
 //root->l->r->l->r->r->r = new Node{11, nullptr, nullptr};
+
+/* ВСЕ НОДЫ НА РАССТОЯНИИ K*/
+void findDown(Node *node, int k, vector<int> &res) {
+    if (!node)
+        return;
+    if (k == 0) {
+        res.push_back(node->data);
+        return;
+    }
+    findDown(node->l, k - 1, res);
+    findDown(node->r, k - 1, res);
+}
+
+bool kdst(Node *node, int val, int &k, vector<int> &res) {
+    if (!node)
+        return false;
+    if (node->data == val) {
+        findDown(node->l, k - 1, res);
+        findDown(node->r, k - 1, res);
+        return true;
+    }
+    auto fl = kdst(node->l, val, k, res);
+    auto fr = kdst(node->r, val, k, res);
+    if (fl) {
+        --k;
+        if (k == 0)
+            res.push_back(node->data);
+        else
+            findDown(node->r, k - 1, res);
+    }
+    if (fr) {
+        --k;
+        if (k == 0)
+            res.push_back(node->data);
+        else
+            findDown(node->l, k - 1, res);
+    }
+    return fl || fr;
+}
+
+vector<int> distanceK(Node *root, Node *target, int K) {
+    vector<int> res;
+    if (K == 0) {
+        res.push_back(target->data);
+        return res;
+    }
+
+    kdst(root, target->data, K, res);
+    return res;
+}
+
+//auto *root = new Node{1, nullptr, nullptr};
+//root->l = new Node{3, nullptr, nullptr};
+//root->r = new Node{2, nullptr, nullptr};
+////
+//root->l->l = new Node{9, nullptr, nullptr};
+//root->l->l->l = new Node{12, nullptr, nullptr};
+//root->l->r = new Node{7, nullptr, nullptr};
+//root->l->r->l = new Node{8, nullptr, nullptr};
+//root->l->r->l->r = new Node{6, nullptr, nullptr};
+//root->l->r->l->r->r = new Node{11, nullptr, nullptr};
+////
+//root->r->r = new Node{10, nullptr, nullptr};
+//root->r->l = new Node{4, nullptr, nullptr};
+//root->r->l->r = new Node{5, nullptr, nullptr};
+//auto res = distanceK(root, root->l->r, 3);
+//for(auto n: res)
+//cout<<n<<' ';
