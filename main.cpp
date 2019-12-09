0d2a5a51f00d165cb6d9c10807b54e5a111faa60
kek228 <xyets777@gmail.com>
2019-12-09 20:42:00 +0300

    сделал про пирамидку

--- main.cpp
+++ main.cpp
@@ -13,41 +13,28 @@
 #include <fstream>
 
 using namespace std;
-size_t bricksGame(vector<int> arr) {
-    auto n = arr.size();
-    if (arr.size() <= 3) {
-        int res = 0;
-        for (auto n: arr) {
-            res += n;
+
+int minimumTotal(vector<vector<int>> &triangle) {
+    auto rows = triangle.size();
+    auto &prevRow = triangle[rows - 1];
+    for (int row = rows - 2; row >= 0; --row) {
+        auto &curRow = triangle[row];
+        for (int j = 0; j < curRow.size(); ++j) {
+            curRow[j] += min(prevRow[j], prevRow[j + 1]);
         }
-        return res;
+        prevRow = curRow;
     }
-    // решение для подзадачи [i:n]
-    vector<size_t> res(n + 1, 0);
-    // в какой индекс надо идти из позиции i
-    vector<size_t> steps(n + 1, 0);
-    int c = 3;
-    int i = n - 1;
-    for (; c > 0; --i) {
-        res[i] += res[i + 1] + arr[i];
-        steps[i] = n;
-        --c;
-    }
-    for (; i >= 0; --i) {
-        auto minIter = min_element(res.begin() + i + 1, res.begin() + i + 4);
-        auto minId = minIter - res.begin();
-        res[i] = res[steps[minId]];
-        for (int j = i; j < minId; ++j)
-            res[i] += arr[j];
-        steps[i] = minId;
-    }
-    return res[0];
+    return triangle[0][0];
 }
 
+
 int main() {
-    //cout<<bricksGame({321, 386, 740, 595, 161, 176, 606, 64, 577, 316});
-    cout << std::numeric_limits<int>::max();
+    vector<vector<int>> triangle = {
+            {2},
+            {3, 4},
+            {6, 5, 7},
+            {4, 1, 8, 3}
+    };
+    cout << minimumTotal(triangle);
     return 0;
 }
-// 249 791 261 588
-//   2 147 483 647
