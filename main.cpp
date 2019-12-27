e8c39860d6f4ec7a800c4720b2a3b45f960e4c0b
kek228 <xyets777@gmail.com>
2019-12-27 16:21:47 +0400

    longest-palindromic-subsequence

--- main.cpp
+++ main.cpp
@@ -22,80 +22,41 @@ vector<vector<int>> constructTable(int rows, int cols) {
     return table;
 }
 
-int processDiag(int row, int col, int l, int k, vector<vector<int>> &table) {
-    int zeroCounter = 0;
-    int b = 0;
-    int e = 1;
-    int res = 0;
-    // e на 1 вперед
-    while (e < l) {
-        if (table[row + e - 1][col + e - 1] == 0)
-            ++zeroCounter;
-        int segmentLength = e - b;
-        if (segmentLength > res && zeroCounter <= k)
-            res = segmentLength;
-        //
-        if (zeroCounter > k) {
-            while (b < e && zeroCounter > k) {
-                ++b;
-                if (table[row + b - 1][col + b - 1] == 0)
-                    --zeroCounter;
-            }
-        }
-        ++e;
+int longestPalindromeSubseq(string s) {
+    int size = s.size();
+    if (size == 0)
+        return 0;
+    if (size == 1)
+        return 1;
+    auto resTable = constructTable(size, size);
+    resTable[0][0] = 1;
+    for (int col = 1; col < size; ++col) {
+        resTable[0][col] = 1;
+        if (s[col] == s[col - 1])
+            resTable[1][col] = 2;
+        else
+            resTable[1][col] = 1;
     }
-    if (table[row + e - 1][col + e - 1] == 0)
-        ++zeroCounter;
-    int segmentLength = e - b;
-    if (segmentLength > res && zeroCounter <= k)
-        res = segmentLength;
-    return res;
+    //
+    for (int row = 2; row < size; ++row) {
+        for (int col = row; col < size; ++col) {
+            if (s[col] == s[col - row])
+                resTable[row][col] = resTable[row - 2][col - 1] + 2;
+            else
+                resTable[row][col] = max(resTable[row - 1][col - 1], resTable[row - 1][col]);
+        }
+    }
+    return resTable[size - 1][size - 1];
 }
 
-int substringDiff(int k, string s1, string s2) {
-    int rows = s1.size();
-    int cols = s2.size();
-    auto table = constructTable(rows, cols);
-    //
-    for (int col = 0; col < cols; ++col) {
-        if (s1[0] == s2[col]) {
-            table[0][col] = 1;
-        }
-    }
-    //
-    for (int row = 1; row < rows; ++row) {
-        if (s2[0] == s1[row]) {
-            table[row][0] = 1;
-        }
-    }
-    //
-    for (int row = 1; row < rows; ++row) {
-        for (int col = 1; col < cols; ++col) {
-            if (s2[col] != s1[row])
-                table[row][col] = 0;
-            else {
-                table[row][col] = table[row - 1][col - 1] + 1;
-            }
-        }
-    }
-    //
-    int res = 0;
-    for (int row = rows - 1; row >= 0; --row) {
-        int curRes = processDiag(row, 0, rows - row, k, table);
-        if (curRes > res)
-            res = curRes;
-    }
-    for (int col = 1; col < cols; ++col) {
-        int curRes = processDiag(0, col, cols - col, k, table);
-        if (curRes > res)
-            res = curRes;
-    }
-    return res;
-}
 
 int main() {
-    cout << substringDiff(2, "torino", "tabriz");
-    //cout << substringDiff(0, "abacba", "abcaba");
-    //cout << substringDiff(3, "helloworld", "yellomarin");
+    //cout<<longestPalindromeSubseq("kecampaep");
+    // cout<<longestPalindromeSubseq("abbkeb");
+    // cout<<longestPalindromeSubseq("dkecampaed");
+    // cout<<longestPalindromeSubseq("bbbab");
+    //cout<<longestPalindromeSubseq("aabaa");
+    cout << longestPalindromeSubseq("abcdabcdabcdabcd");
+
     return 0;
 }
