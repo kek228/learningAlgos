ad4106556723154d21014caa766c1efa87e53891
kek228 <xyets777@gmail.com>
2019-12-30 12:14:23 +0400

    решал https://www.hackerrank.com/challenges/prime-digit-sums/problem
    НЕ решил, надо разобрать

--- AlgUtils.h
+++ AlgUtils.h
@@ -6,20 +6,37 @@
 #include <queue>
 #pragma once
 using namespace std;
-namespace alg_utils{
+namespace alg_utils {
     // парсинг строки в вектор чисел
-    vector<int> split(const string& str, char delim = ' '){
+    vector<int> split(const string &str, char delim = ' ') {
         vector<int> res;
         std::stringstream ss(str);
         std::string token;
         while (std::getline(ss, token, delim)) {
-            res.push_back(token);
+            res.push_back(stoi(token)
+            _;
         }
     }
+
+    // из файлика
+    vector<int> split(char delim = ' ') {
+        std::ifstream t("/Users/nikita/CLionProjects/ALGOS/test.txt");
+        std::string str((std::istreambuf_iterator<char>(t)),
+                        std::istreambuf_iterator<char>());
+
+        vector<int> res;
+        std::stringstream ss(str);
+        std::string token;
+        while (std::getline(ss, token, delim)) {
+            res.push_back(stoi(token));
+        }
+        return res;
+    }
+
     // все цифры в числе
-    vector<int> getUnorderedDigits(string &s){
+    vector<int> getUnorderedDigits(string &s) {
         bool digits[10] = {};
-        for(auto c: s){
+        for (auto c: s) {
             digits[c - '0'] = true;
         }
         vector<int> res;
--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include <fstream>
 #include <set>
 
+// 0 1 2 3 5 7 11 13 17 19 23 29 31 37 41 43
+
 using namespace std;
 
 vector<vector<int>> constructTable(int rows, int cols) {
@@ -22,38 +24,124 @@ vector<vector<int>> constructTable(int rows, int cols) {
     return table;
 }
 
-vector<int> split(char delim = ' ') {
-    std::ifstream t("/Users/nikita/CLionProjects/ALGOS/test.txt");
-    std::string str((std::istreambuf_iterator<char>(t)),
-                    std::istreambuf_iterator<char>());
+bool prime(int n) {
+    if (n == 0)
+        return false;
+    if (n == 1)
+        return true;
+    for (int x = 2; x * x <= n; ++x) {
+        if (n % x == 0) return false;
+    }
+    return true;
+}
 
-    vector<int> res;
-    std::stringstream ss(str);
-    std::string token;
-    while (std::getline(ss, token, delim)) {
-        res.push_back(stoi(token));
+string vtostr(vector<int> &permut) {
+    string res;
+    for (int i = 0; i < permut.size(); ++i) {
+        char c = '0' + permut[i];
+        res.push_back(c);
     }
     return res;
 }
 
-int cost(vector<int> b) {
-    int size = b.size();
-    auto table = constructTable(2, size);
-    for (int i = 1; i < size; ++i) {
-        // решение заканчивается на 1
-        table[0][i] = max(table[0][i - 1] + 0, table[1][i - 1] + abs(b[i - 1] - 1));
-        // решение заканчивается на макс возможное
-        table[1][i] = max(table[0][i - 1] + abs(b[i] - 1), table[1][i - 1] + abs(b[i] - b[i - 1]));
+
+class PrimeDigitSums {
+public:
+    PrimeDigitSums() {
+        vector<int> permut;
+        genAllPermutsWithDoubles(permut, 0, 10, 5);
     }
-    return max(table[0][size - 1], table[1][size - 1]);
+
+    int primeDigitSums(int n) {
+        int res = 0;
+        // возможные комбинации с прошлой пятерки
+        unordered_set<string> lastStepPermuts;
+        for (const auto &permut: valid5Nums) {
+            if (permut[0] != '0')
+                lastStepPermuts.insert(permut);
+        }
+        //
+        int step = 6;
+        while (step <= n) {
+            lastStepPermuts = genNewPermuts(lastStepPermuts);
+            res += lastStepPermuts.size();
+            ++step;
+        }
+        return res;
+    }
+
+private:
+    unordered_set<string> genNewPermuts(unordered_set<string> &lastStepPermuts) {
+        unordered_set<string> newPermuts;
+        for (const auto &permut: lastStepPermuts) {
+            for (int i = 0; i < 10; ++i) {
+                auto newPermut = permut.substr(1);
+                newPermut.push_back('0' + i);
+                if (valid5Nums.find(newPermut) != valid5Nums.end()) {
+                    // cout<<permut[0]<<newPermut.data()<<endl;
+                    newPermuts.insert(newPermut);
+                }
+            }
+        }
+        return newPermuts;
+    }
+
+
+    bool checkPrime(vector<int> &permut) {
+        int sum = 0;
+        for (int i = 0; i < 3; ++i)
+            sum += permut[i];
+        if (!prime(sum))
+            return false;
+        //
+        sum -= permut[0];
+        sum += permut[3];
+        if (!prime(sum))
+            return false;
+        //
+        sum -= permut[1];
+        sum += permut[4];
+        if (!prime(sum))
+            return false;
+        //
+        sum = 0;
+        for (int i = 0; i < 4; ++i)
+            sum += permut[i];
+        if (!prime(sum))
+            return false;
+        sum -= permut[0];
+        sum += permut[4];
+        if (!prime(sum))
+            return false;
+        //
+        sum += permut[0];
+        return prime(sum);
+    }
+
+    void genAllPermutsWithDoubles(vector<int> &permut, int from, int to, int permutsize) {
+        if (permut.size() == permutsize) {
+            if (checkPrime(permut)) {
+                valid5Nums.insert(vtostr(permut));
+            }
+            return;
+        }
+        for (int j = from; j < to; ++j) {
+            permut.push_back(j);
+            genAllPermutsWithDoubles(permut, from, to, permutsize);
+            permut.pop_back();
+        }
+    }
+
+    unordered_set<string> valid5Nums;
+};
+
+int primeDigitSums(int n) {
+    PrimeDigitSums sumsHandler;
+    return sumsHandler.primeDigitSums(n);
 }
 
 
 int main() {
-    // cout << cost({100, 10, 5});
-    // cout << cost({1, 2, 3});
-    // 10 62 30 19 71 49 13 40 16 44 28 -> 426
-    auto b = split();
-    cout << cost({100, 2, 100, 2, 100});
+    cout << primeDigitSums(6);
     return 0;
 }
