f531a16370c6caa7387d114226aa1d9cade9aa85
kek228 <xyets777@gmail.com>
2019-12-31 20:41:04 +0400

    https://leetcode.com/problems/delete-and-earn/ По сути задача на макс последовательность в которой нельзя брать 2 подряд. С Новым Годом

--- main.cpp
+++ main.cpp
@@ -17,53 +17,67 @@
 
 using namespace std;
 
-vector<vector<int>> constructTable(int rows, int cols) {
-    vector<vector<int>> table(rows);
-    for (auto &row: table)
-        row = vector<int>(cols, 0);
-    return table;
-}
-
-
-int maxProfit(vector<int> &prices) {
-#define BUY 0
-#define SELL 1
-#define BUY_WAIT 2
-#define SELL_WAIT 3
-#define WAIT 4
-
-    if (prices.size() < 2)
+int deleteAndEarn(vector<int> &nums) {
+    if (nums.size() == 0)
         return 0;
-    int cols = static_cast<int>(prices.size());
-    int rows = 5;
-    auto table = constructTable(rows, cols);
-    for (int row = 0; row < 5; ++row) {
-        table[row][0] = numeric_limits<int>::min();
+
+    int N = *max_element(nums.begin(), nums.end());
+    vector<int> counters(N + 1, 0);
+    for (auto n : nums) {
+        ++counters[n];
     }
-    table[BUY][0] = -prices[0];
-    table[WAIT][0] = 0;
-    for (int col = 1; col < cols; ++col) {
-        table[BUY][col] = max(table[SELL_WAIT][col - 1], table[WAIT][col - 1]) - prices[col];
-        //
-        table[SELL][col] = max(table[BUY][col - 1], table[BUY_WAIT][col - 1]) + prices[col];
-        //
-        table[BUY_WAIT][col] = max(table[BUY][col - 1], table[BUY_WAIT][col - 1]);
-        //
-        table[SELL_WAIT][col] = max(table[SELL][col - 1], table[SELL_WAIT][col - 1]);
-        //
-        table[WAIT][col] = 0;
+    vector<pair<int, int>> groups;
+    int res = 0;
+    int start = 0;
+    while (start < N + 1) {
+        if (counters[start] != 0) {
+            if (start != N && counters[start + 1] != 0) {
+                int j = start + 1;
+                while (j < N && counters[j] != 0)
+                    ++j;
+                groups.push_back({start, j});
+                start = j + 1;
+            } else {
+                res += start * counters[start];
+                ++start;
+            }
+        } else {
+            ++start;
+        }
     }
-    int res = table[0][cols - 1];
-    for (int row = 0; row < 5; ++row) {
-        if (table[row][cols - 1] > res)
-            res = table[row][cols - 1];
+    //
+    for (auto &g: groups) {
+        int pp = counters[g.first] * (g.first);
+        int p = counters[g.first + 1] * (g.first + 1);
+        if (p < pp)
+            p = pp;
+        int curRes = max(p, pp);
+        for (int i = g.first + 2; i <= g.second; ++i) {
+            int add = counters[i] * (i);
+            if (add + pp > p) {
+                int _prev = add + pp;
+                pp = p;
+                p = _prev;
+            } else {
+                pp = p;
+            }
+            curRes = p;
+        }
+        res += curRes;
     }
     return res;
 }
 
+
 int main() {
-    // vector<int> prices = {1, 2, 3, 0, 2};
-    vector<int> prices = {1, 2, 4};
-    cout << maxProfit(prices);
+    vector<int> nums = {10, 8, 4, 2, 1, 3, 4, 8, 2, 9, 10, 4, 8, 5, 9, 1, 5, 1, 6, 8, 1, 1, 6, 7, 8, 9, 1, 7, 6, 8, 4,
+                        5, 4, 1, 5, 9, 8, 6, 10, 6, 4, 3, 8, 4, 10, 8, 8, 10, 6, 4, 4, 4, 9, 6, 9, 10, 7, 1, 5, 3, 4, 4,
+                        8, 1, 1, 2, 1, 4, 1, 1, 4, 9, 4, 7, 1, 5, 1, 10, 3, 5, 10, 3, 10, 2, 1, 10, 4, 1, 1, 4, 1, 2,
+                        10, 9, 7, 10, 1, 2, 7, 5};
+    cout << deleteAndEarn(nums) << endl;
+    nums = {3, 4, 2};
+    cout << deleteAndEarn(nums) << endl;
+    nums = {1};
+    cout << deleteAndEarn(nums) << endl;
     return 0;
 }
