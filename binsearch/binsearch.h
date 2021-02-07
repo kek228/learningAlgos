9eee01bfb08b5ee8551f7926718ffc09ca459d05
kek228 <xyets777@gmail.com>
2021-02-07 10:15:10 +0000

    https://leetcode.com/problems/search-in-rotated-sorted-array/

--- binsearch/binsearch.h
+++ binsearch/binsearch.h
@@ -247,3 +247,29 @@ int searchInsert(vector<int> &nums, int target) {
         return 0;
     return e;
 }
+
+//https://leetcode.com/problems/search-in-rotated-sorted-array/
+int search(vector<int> &nums, int target) {
+    if (nums.size() == 0)
+        return 0;
+    int l = 0;
+    int r = nums.size() - 1;
+    while (l <= r) {
+        int m = (l + r) / 2;
+        if (nums[m] == target)
+            return m;
+        if (nums[m] >= nums[l]) {
+            if (target >= nums[l] && target < nums[m])
+                r = m - 1;
+            else
+                l = m + 1;
+        } else if (nums[m] <= nums[r]) {
+            if (target > nums[m] && target <= nums[r])
+                l = m + 1;
+            else
+                r = m - 1;
+
+        }
+    }
+    return -1;
+}
--- main.cpp
+++ main.cpp
@@ -23,5 +23,7 @@
 using namespace std;
 
 int main() {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+    cout << search(nums, 0);
     return 0;
 }
